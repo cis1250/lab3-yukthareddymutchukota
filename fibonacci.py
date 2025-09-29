c21b188cd2c2209d1cb1db20364dee493f6d15b3
Yuktha Reddy Mutchukota <ymutchuk@uoguelph.ca>
2025-09-29 11:53:52 -0400

    initial code for fibonacci assignment

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 # Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
+# TODO: (Read detailed instructions in the Readme file) ✓
+# Prompt the user for the number of terms. ✓
+numterms = int(input("Enter the Number of Terms Wanted for Fibonacci Sequence: "))
+
+# Validate that the input is a positive integer. ✓
+while numterms <= 0 :
+  int(input(f"Oh no! The number you have entered is not a positive integer. Please try again. \nEnter the Number of Terms Wanted for Fibonacci Sequence: "))
+
+# Initialize variables for fibonacci sequence computation.
+a, b = 0, 1
+
+print(f"\nFibonacci Sequence for {numterms} terms: ")
+
+# Use a for loop to print the Fibonacci sequence up to that many terms. ✓
+for x in range(numterms) :
+  print(a, end=" ")
+  a, b = b, a + b
