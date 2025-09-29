1b91553d06709ff3eb9fc7d3e6774fdbf9c4b9d3
Yuktha Reddy Mutchukota <ymutchuk@uoguelph.ca>
2025-09-29 16:45:06 +0000

    finalized fibonacci code

--- fibonacci.py
+++ fibonacci.py
@@ -2,12 +2,21 @@
 
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file) ✓
-# Prompt the user for the number of terms. ✓
-numterms = int(input("Enter the Number of Terms Wanted for Fibonacci Sequence: "))
+# Initalize y to True to enter while loop.
+y = True
 
-# Validate that the input is a positive integer. ✓
-while numterms <= 0 :
-  int(input(f"Oh no! The number you have entered is not a positive integer. Please try again. \nEnter the Number of Terms Wanted for Fibonacci Sequence: "))
+# Prompt the user for the number of terms. ✓
+while y == True:    
+  numterms = input("Enter the Number of Terms Wanted for Fibonacci Sequence: ")
+    
+  # Validate that the input is a positive integer. ✓
+  if numterms.isdigit() and int(numterms) > 0:
+      # Safe to convert to integer and break.
+      numterms = int(numterms)  
+      y == False
+      break
+  else:
+    print("Oh no! What you have entered is not a positive integer. Please try again.\n")
 
 # Initialize variables for fibonacci sequence computation.
 a, b = 0, 1
@@ -15,6 +24,6 @@ a, b = 0, 1
 print(f"\nFibonacci Sequence for {numterms} terms: ")
 
 # Use a for loop to print the Fibonacci sequence up to that many terms. ✓
-for x in range(numterms) :
+for x in range(int(numterms)) :
   print(a, end=" ")
   a, b = b, a + b
